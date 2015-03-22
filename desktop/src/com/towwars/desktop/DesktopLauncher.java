fd82ddd95caaf509426834bf6891b58590755b32
Technomeow <iamtechnocat@gmail.com>
2015-03-22 14:46:10 +0300

    init

---
+++ desktop/src/com/towwars/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.towwars.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.towwars.TWarsGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TWarsGame(), config);
+	}
+}
